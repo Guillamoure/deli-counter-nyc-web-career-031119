475235afb37f3e5d71a61efb156b63776d1d4f66
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-02-25 03:07:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,10 +3,11 @@ def line(array)
   if array.any? == false
     puts "The line is currently empty."
   else
-    current_line = "The line is currently: "
+    current_line = "The line is currently:"
     position = 1
     array.each do |name|
-      array.push("#{position}. #{name} ")
+      array.push(" #{position}. #{name}")
     end
+    puts current_line
   end
 end
