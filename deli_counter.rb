61fc689ca4236e5a4d705182b9e6a7dd55e5fa58
cool-invocation-5581 <jhilscheriv@gmail.com>
2019-02-25 03:01:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,9 @@
 # Write your code here.
 def line(array)
   array.any?
-  if TRUE
-    puts array
-  elsif FALSE
+  if false
+    puts "The line is currently empty"
+  elsif true
     puts "The line is currently empty."
   end
 end
